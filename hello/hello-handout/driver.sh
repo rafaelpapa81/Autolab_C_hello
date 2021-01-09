b0b86e2132a65ab00881db7470bf7524b7d01cc2
Rafael Papa <rafaelpapa81@gmail.com>
2021-01-09 18:07:08 -0500

    Add semantic feedback

--- hello/hello-handout/driver.sh
+++ hello/hello-handout/driver.sh
@@ -11,7 +11,7 @@ echo "Compiling hello.c"
 status=$?
 if [ ${status} -ne 0 ]; then
     echo "Failure: Unable to compile hello.c (return status = ${status})"
-	echo "{\"_presentation\": \"semantic\"}"
+	echo "{\"_presentation\": \"semantic\", \"stages\": [\"Test Results\"], \"Test Results\": {\"Build\": {\"passed\": false}, \"Run\": {\"passed\": false}}}"
     echo "{\"scores\": {\"Correctness\": 0}}"
     exit
 fi
@@ -22,13 +22,12 @@ echo "Running ./hello"
 status=$?
 if [ ${status} -eq 0 ]; then
     echo "Success: ./hello runs with an exit status of 0"
-	echo "{\"_presentation\": \"semantic\"}"
+	echo "{\"_presentation\": \"semantic\", \"stages\": [\"Test Results\"], \"Test Results\": {\"Build\": {\"passed\": true}, \"Run\": {\"passed\": true}}}"
     echo "{\"scores\": {\"Correctness\": 100}}"
 else
     echo "Failure: ./hello fails or returns nonzero exit status of ${status}"
-	echo "{\"_presentation\": \"semantic\"}"
-    echo "{\"scores\": {\"Correctness\": 0}}"
+	echo "{\"_presentation\": \"semantic\", \"stages\": [\"Test Results\"], \"Test Results\": {\"Build\": {\"passed\": true}, \"Run\": {\"passed\": false, \"hint\": \"You need to return 0\"}}}"
+    echo "{\"scores\": {\"Correctness\": 70}}"
 fi
 
 exit
-
